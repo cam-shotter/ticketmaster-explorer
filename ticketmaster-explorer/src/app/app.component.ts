783c33b025e91802cc43268b13aa54d2785891ed
Cam Shotter <cameronshotter@gmail.com>
2023-04-20 21:19:04 +1200

    setting up very basic interfaces for example event types, and displaying very basic list of mock events on page

--- ticketmaster-explorer/src/app/app.component.ts
+++ ticketmaster-explorer/src/app/app.component.ts
@@ -1,4 +1,22 @@
+import { Time } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface EventDateTime {
+  start: Date;
+  end: Date;
+  format?: string; // Todo create enum of accepted DateTime formats
+}
+
+interface EventDetails {
+  eventDateTimes: EventDateTime[];
+  prices: number[];
+  venues: string[];
+  locations: string[];
+}
+interface Event extends EventDetails {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'ticketmaster-explorer-root',
@@ -7,4 +25,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ticketmaster-explorer';
+  events: Event[] = [
+    {
+      id: 1,
+      name: 'Event 1',
+      eventDateTimes: [{
+        start: new Date('2021-01-01 10:00'),
+        end: new Date('2021-01-01 12:00'),
+      }],
+      prices: [10],
+      venues: ['Venue 1'],
+      locations: ['Location 1'],
+    },
+    {
+      id: 2,
+      name: 'Event 2',
+      eventDateTimes: [{
+        start: new Date('2021-01-02 10:00'),
+        end: new Date('2021-01-02 12:00'),
+      }],
+      prices: [20],
+      venues: ['Venue 2'],
+      locations: ['Location 2'],
+    }
+  ]
 }
