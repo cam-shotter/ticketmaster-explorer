4d6b3219552a7cb226f7b16eeda777ed8a058e83
Cam Shotter <cameronshotter@gmail.com>
2023-04-24 22:37:19 +1200

    Imported matbutton

--- ticketmaster-explorer/src/app/material.module.ts
+++ ticketmaster-explorer/src/app/material.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatTableModule } from '@angular/material/table'; 
+import {MatButtonModule} from '@angular/material/button'; 
 
 
 @NgModule({
@@ -12,11 +13,13 @@ import { MatTableModule } from '@angular/material/table';
     MatCardModule,
     MatGridListModule,
     MatTableModule,
+    MatButtonModule,
   ],
   exports: [
     MatCardModule,
     MatGridListModule,
     MatTableModule,
+    MatButtonModule,
   ]
 })
 export class MaterialModule { }
