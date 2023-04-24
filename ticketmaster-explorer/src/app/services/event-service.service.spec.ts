fc05f947ac2a84fcdbb0218ed171505773bb3d09
Cam Shotter <cameronshotter@gmail.com>
2023-04-24 21:55:52 +1200

    Added basic test to service, and created interfaces to type the response

--- ticketmaster-explorer/src/app/services/event-service.service.spec.ts
+++ ticketmaster-explorer/src/app/services/event-service.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EventService } from './event-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('EventService', () => {
   let service: EventService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(EventService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('getEvents', () => {
+    describe('given valid parameters', () => {
+      it('should not return null response', (done) => {
+          const keyword = "Metallica";
+          const location = "37.566535,126.9779692";
+          const startDatetime = new Date("2019-09-01T00:00:00");
+          const endDatetime = new Date("2019-09-30T00:00:00");
+          service.getEvents(keyword, location, startDatetime, endDatetime).subscribe(data => {
+              expect(data).not.toBeNull();
+              done();
+          });
+      })
+    });
+})
 });
--- ticketmaster-explorer/src/app/services/event-service.service.ts
+++ ticketmaster-explorer/src/app/services/event-service.service.ts
@@ -1,6 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+export interface Event {
+  name: string,
+  type: string,
+  id: string,
+  test: boolean,
+  url: string,
+  locale: string,
+}
+
+export interface Links {
+  self: {
+    href: string
+  },
+  next: {
+    href: string
+  },
+  last: {
+    href: string
+  },
+  first: {
+    href: string
+  }
+}
+
+export interface Page {
+  size: number,
+  totalElements: number,
+  totalPages: number,
+  number: number
+}
+
+export interface GetEventResponse {
+  _embedded: {
+    events: Event[],
+  },
+  page: Page,
+  _links: LinkStyle,
+}
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +60,7 @@ export class EventService {
     params = params.append('endDateTime', endDatetime.toISOString().slice(0, -5) + 'Z');
     params = params.append('size', size);
     params = params.append('page', page);
-    
-    return this.http.get<any>(this.apiUrl, { params });
+
+    return this.http.get<GetEventResponse>(this.apiUrl, { params });
   }
 }
